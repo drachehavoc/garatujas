793ba9b58e855698ac543caf9933e7c7f9a8c64b
Daniel de Andrade Varela <1902294+drachehavoc@users.noreply.github.com>
2025-12-23 09:58:57 -0300

    Update ts:type -> permitir instancia receber novos métodos.ts

--- ts:type -> permitir instancia receber novos métodos.ts
+++ ts:type -> permitir instancia receber novos métodos.ts
@@ -1,9 +1,9 @@
 type BaseMais<T, Opts extends {}> = T & {
-	// K é string
-	// K extende chaves de T
-	// - se sim: K tem o mesmo tipo de K existente em T
-	// - se não: K é do tipo `(opts: Opts) => any` ou `indefinido`
-	[K in string]: K extends keyof T ? T[K] : ((opts: Opts) => any) | undefined;
+	// [K in string]                       // K é string
+	// : K extends keyof T ? T[K]          // K extende chaves de T?
+	// ? T[K]                              // - se sim: K tem o mesmo tipo de K existente em T
+	// : ((opts: Opts) => any) | undefined //- se não: K é do tipo `(opts: Opts) => any` ou `indefinido`
+	[K in string]: K extends keyof T ? T[K] : ((opts: Opts) => any) | undefined
 }
 
 const canInstantiateWithNew = Symbol()
